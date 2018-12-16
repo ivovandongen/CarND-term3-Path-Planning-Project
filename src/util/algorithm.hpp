062fbbf8cabb5fd11a338f3f4f21e9c81f55eec5
Ivo van Dongen <info@ivovandongen.nl>
2018-12-16 21:55:14 +0200

    add util for wrapped diff

--- src/util/algorithm.hpp
+++ src/util/algorithm.hpp
@@ -1,12 +1,21 @@
 #pragma once
 
 #include <algorithm>
+#include <cmath>
 
 namespace util {
 
 template<class T>
 T clamp(T input, T lowerBounds, T upperBounds) {
     return std::max<T>(std::min<T>(input, upperBounds), lowerBounds);
+}
+
+template<class T>
+T diff_wrapped(T a, T b, T max) {
+    return std::min(
+            std::abs(std::fmod(a, max) - std::fmod(b, max)),
+            std::abs(std::fmod(a, max) + max - std::fmod(b, max))
+    );
 }
 
 } // namespace util
